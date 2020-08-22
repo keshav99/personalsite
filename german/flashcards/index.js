0e3e4b4df19e93a3be64ac5a5e080d3464c49c06
keshav-algonox <keshav.moorthy1999@gmail.com>
2020-08-22 22:22:59 +0530

    Adding german lang app

--- german/flashcards/index.js
+++ german/flashcards/index.js
@@ -1,4 +1,26 @@
-var output = [];
+
+
+var loadWord = function(){
+    var ran = (Math.floor(Math.random() * output.length) + 1)-1;
+
+    var newword = output[ran];
+    var sentences,color;
+    for(var i=0; i<output.length; i++){
+        if(output[i].word == newword){
+            sentences = output[i].sentences;
+            color = output[i].color;
+            break;
+        }
+    }
+    console.log(ran+" "+newword+" "+sentences+" "+color)
+    $("#wordName").val = newword;
+    sentences.forEach(e => {
+        $("#sentences").append('<li>'+e+'</li>');
+    });
+}
+
+$(window).load(function() {
+    var output = [];
 var colors = ["#5e65db", "#a448d9", "#d94871", "#d97148", "#cad948", "#48d973"]
 $.getJSON( "./wordlist.json", function( data ) {
     console.log( data);
@@ -13,24 +35,5 @@ $.getJSON( "./wordlist.json", function( data ) {
   });
   console.log("Printing output");
   console.log(output);
-
-var loadWord = function(){
-    var newword = output[(Math.floor(Math.random() * output.length) + 1)-1];
-    var sentences,color;
-    for(var i=0; i<output.length; i++){
-        if(output[i].word == newword){
-            sentences = output[i].sentences;
-            color = output[i].color;
-            break;
-        }
-    }
-    console.log(newword+" "+sentences+" "+color)
-    $("#wordName").val = newword;
-    sentences.forEach(e => {
-        $("#sentences").append('<li>'+e+'</li>');
-    });
-}
-
-$(window).load(function() {
     loadWord();
 });
