d5f97a37711a31e96c2d302ea8f8aa7cff85d4ce
keshav-algonox <keshav.moorthy1999@gmail.com>
2020-08-22 22:39:16 +0530

    Adding german lang app

--- german/flashcards/index.js
+++ german/flashcards/index.js
@@ -1,19 +1,21 @@
 
 
-var loadWord = function(output){
+var loadWord = function(output, words){
     console.log(output);
     var ran = (Math.floor(Math.random() * 627) + 1)-1;
 
-    var newword = output[ran];
+    var newword = words[ran];
     var sentences,color;
-    for(var i=0; i<627; i++){
-        console.log(output[i]);
-        if(output[i].word == newword){
-            sentences = output[i].sentences;
-            color = output[i].color;
-            break;
-        }
-    }
+    // for(var i=0; i<627; i++){
+    //     console.log(output[i]);
+    //     if(output[i].word == newword){
+    //         sentences = output[i].sentences;
+    //         color = output[i].color;
+    //         break;
+    //     }
+    // }
+    sentences = output[words][0];
+    color = output[words][1];
     console.log(ran+" "+newword+" "+sentences+" "+color)
     $("#wordName").val = newword;
     sentences.forEach(e => {
@@ -22,17 +24,20 @@ var loadWord = function(output){
 }
 
 $(window).load(function() {
-    var output = [];
-var colors = ["#5e65db", "#a448d9", "#d94871", "#d97148", "#cad948", "#48d973"]
+    var output = {};
+var words = {}
+    var colors = ["#5e65db", "#a448d9", "#d94871", "#d97148", "#cad948", "#48d973"]
 $.getJSON( "./wordlist.json", function( data ) {
 
     $.each(data, function(key, val) {
-        output.push({
-            word: key,
-            sentences: val,
-            color: colors[(Math.floor(Math.random() * 6) + 1)-1]
-        })
+        // output.push({
+        //     word: key,
+        //     sentences: val,
+        //     color: colors[(Math.floor(Math.random() * 6) + 1)-1]
+        // })
+        output[key] = [val,colors[(Math.floor(Math.random() * 6) + 1)-1]];
+        words.append(key)
      })
   });
-    loadWord(output);
+    loadWord(output, words);
 });
