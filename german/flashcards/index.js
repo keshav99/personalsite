914d429e6df0e4cb07646844f189339ae797499e
keshav-algonox <keshav.moorthy1999@gmail.com>
2020-08-22 19:44:24 +0530

    Adding german lang app

--- german/flashcards/index.js
+++ german/flashcards/index.js
@@ -2,10 +2,8 @@ var output;
 $.getJSON( "./wordlist.json", function() {
     console.log( "success" );
   })
-  .done(function(data){
-      output = data;
-  })
-
-  $.each(output.items, function(i, item){
-    console.log(item);
-  })
+  .done(function( data ) {
+    $.each( data.items, function( i, item ) {
+      print(item);
+    });
+  });
