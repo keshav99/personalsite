4658c3cded224fdc98ccaa0d63bf88ffd1651ed5
keshav-algonox <keshav.moorthy1999@gmail.com>
2020-08-22 22:00:21 +0530

    Adding german lang app

--- german/flashcards/index.js
+++ german/flashcards/index.js
@@ -1,7 +1,15 @@
-var output;
+var output = [];
+var colors = ["#5e65db", "#a448d9", "#d94871", "#d97148", "#cad948", "#48d973"]
 $.getJSON( "./wordlist.json", function( data ) {
     console.log( data);
     $.each(data, function(key, val) {
         console.log(key+" "+val);
+        output.push({
+            word: key,
+            sentences: val,
+            color: colors[(Math.floor(Math.random() * 6) + 1)-1]
+        })
      })
   });
+  console.log("Printing output");
+  console.log(output);
