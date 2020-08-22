72484ffb1314eb21252722f4e90918b9a8de90d8
keshav-algonox <keshav.moorthy1999@gmail.com>
2020-08-22 22:31:30 +0530

    Adding german lang app

--- german/flashcards/index.js
+++ german/flashcards/index.js
@@ -2,11 +2,11 @@
 
 var loadWord = function(output){
     console.log(output);
-    var ran = (Math.floor(Math.random() * output.length) + 1)-1;
+    var ran = (Math.floor(Math.random() * 627) + 1)-1;
 
     var newword = output[ran];
     var sentences,color;
-    for(var i=0; i<output.length; i++){
+    for(var i=0; i<627; i++){
         if(output[i].word == newword){
             sentences = output[i].sentences;
             color = output[i].color;
@@ -26,7 +26,6 @@ var colors = ["#5e65db", "#a448d9", "#d94871", "#d97148", "#cad948", "#48d973"]
 $.getJSON( "./wordlist.json", function( data ) {
 
     $.each(data, function(key, val) {
-        console.log(key+" "+val);
         output.push({
             word: key,
             sentences: val,
