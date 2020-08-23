01300b4439b84d72c6ce559ee6ac061619b588b4
keshav-algonox <keshav.moorthy1999@gmail.com>
2020-08-23 12:40:12 +0530

    Adding german lang app

--- german/flashcards/index.js
+++ german/flashcards/index.js
@@ -50,7 +50,7 @@ var loadWord = function(words, sentences, wordcolors){
     s.forEach(e => {
         $("#sentences").append('<li>'+e+'</li>');
     });
-    $("#banner").css('background-color', c);
+    $("#container").css('background-color', c);
     $("#sample").attr("src",i);
 }
 
