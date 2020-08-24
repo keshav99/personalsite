bfa893df924087d54bb57be4f16714b07aca7a7f
keshav-algonox <keshav.moorthy1999@gmail.com>
2020-08-24 21:57:12 +0530

    r

--- german/flashcards/index.js
+++ german/flashcards/index.js
@@ -164,7 +164,7 @@ var loadWord = function(words, sentences, wordcolors){
 
 function getCookie() {
   var result = document.cookie.match(new RegExp("wordVocabForGermanFlashcards" + '=([^;]+)'));
-  result && (result = JSON.parse(JSON.stringify(result[1])));
+  result && (result = result[1]);
   return result;
 } 
 
